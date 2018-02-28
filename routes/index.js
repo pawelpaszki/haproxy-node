94871ec1ff198805ac5505e13fbabbae17d93268
Pawel Paszki <pawelpaszki@gmail.com>
2018-02-28 15:06:41 +0000

    few small changes

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    const ip = req.headers['x-forwarded-for'] ||
-        req.connection.remoteAddress ||
-        req.socket.remoteAddress ||
-        (req.connection.socket ? req.connection.socket.remoteAddress : null);
-    console.log(ip);
-  res.render('index');
+    console.log('x-forwarded-for ' + req.headers['x-forwarded-for']);
+    console.log('remote address ' + req.connection.remoteAddress);
+    console.log('socket remote address  ' + req.socket.remoteAddress);
+    res.render('index');
 });
 
 module.exports = router;
